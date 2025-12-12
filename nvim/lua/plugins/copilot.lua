4272356c8ce0f3f58a6fc456d9dddc3d0b5f22ce
hiro <104716586+hirohsh@users.noreply.github.com>
2025-12-12 22:42:11 +0900

    add config

---
+++ nvim/lua/plugins/copilot.lua
@@ -0,0 +1,25 @@
+-- ~/.config/nvim/lua/plugins/copilot.lua
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-l>",
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        TelescopePrompt = false,
+        gitcommit = false,
+      },
+    },
+  },
+}
